a418f427c5cb32185c6be21066bf90efb73ba6fd
byavv <aksslavvv@gmail.com>
2015-11-18 23:18:21 +0300

    mocha configured, initial test added

---
+++ test/first.spec.js
@@ -0,0 +1,9 @@
+/// <reference path="../typings/tsd.d.ts" />
+var assert = require("assert");
+describe("my feature test", function () {
+    describe("Scenario 1", function () {
+        it("does something expected", function () {
+            assert(true);
+        });
+    });
+});
---
+++ test/first.spec.ts
@@ -0,0 +1,10 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import * as assert from "assert";
+describe("my feature test", () => {
+	describe("Scenario 1", () => {
+		it("does something expected", function(){
+			assert(true);
+		})
+	})
+})
